dd6b407969c427de60c11c3df826bb1e456dafd1
Fleur Kelpin <f.kelpin@umcg.nl>
2016-10-12 12:31:02 +0200

    fix merge

--- src/routes/Directory/components/Directory.js
+++ src/routes/Directory/components/Directory.js
@@ -3,7 +3,7 @@ import classes from './Directory.scss'
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
 
 const propTypes = {
-  queryPayload : PropTypes.string,
+  queryPayload : PropTypes.object,
   items        : PropTypes.object
 }
 
@@ -15,13 +15,11 @@ export const Directory = ({ queryPayload, items }) => {
   return <div className={classes['Directory']}>
     <h4>Directory</h4>
     <h2>items</h2>
-    <BootstrapTable pagination data={items} search>
+    {items && <BootstrapTable pagination data={items} search>
       <TableHeaderColumn isKey dataField='id'>Collection ID</TableHeaderColumn>
       <TableHeaderColumn dataField='name'>Collection name</TableHeaderColumn>
       <TableHeaderColumn dataFormat={format} dataField='biobank'>Biobank ID</TableHeaderColumn>
-
-    </BootstrapTable>
-    <ul />
+    </BootstrapTable>}
     <pre>{JSON.stringify(queryPayload, null, 2)}</pre>
   </div>
 }
--- src/routes/Directory/containers/DirectoryContainer.js
+++ src/routes/Directory/containers/DirectoryContainer.js
@@ -1,4 +1,5 @@
 import { connect } from 'react-redux'
+import { getQueryPayload } from '../modules/Directory'
 
 import Directory from '../components/Directory'
 
