1047e1c4329f0c3068aa89b6d56d6ab3dcfc9a02
Fleur Kelpin <f.kelpin@umcg.nl>
2016-10-12 11:49:29 +0200

    Add selector for query payload

--- src/routes/Directory/modules/Directory.js
+++ src/routes/Directory/modules/Directory.js
@@ -9,7 +9,13 @@ export const getAttributes = (state) => fromEntities.getAttributes(state.entitie
 export const getRsql = (state) => state && state.filters && state.entities &&
   fromFilters.getRsql(state.filters, getAttributes(state))
 export const getHumanReadable = (state) => state && state.filters && state.entities &&
-fromFilters.getHumanReadable(state.filters, getAttributes(state))
+  fromFilters.getHumanReadable(state.filters, getAttributes(state))
+export function getQueryPayload(state) {
+  const url = 'https://molgenis52.gcc.rug.nl/api/v2/eu_bbmri_eric_collections?q=' + getRsql(state)
+  const humanReadable = getHumanReadable(state)
+  const collections = [] //TODO: mark!! fromEntities.getCollections(state.entities)
+  return { url, humanReadable, collections }
+}
 
 export const reducer = combineReducers({ entities, filters })
 
